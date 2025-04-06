1ede6ec66d29f972fc234d4c8a336012b4c44b1e
Cristiano Roberto <cristianoroberto@outlook.com>
2025-04-06 15:02:16 -0300

    Anotações de estudo

---
+++ notas_de_aprendizado.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 1) USUÁRIOS - Na linha de comando o símbolo "$" representa o usuário padrão, e "#" o usuário administrador. 
+# Para alterarmos para o usuário administrador, basta digitar: >>> su root
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# 2) SENHAS - Ao trocar para usuário será necessária a sua senha de administrador. Se você usa Ubuntu linux ou outra distribuição que não 
+# tem o usuário root disponível por padrão, execute o comando: >>> sudo passwd root
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# 3) SHELL - Como saber qual o Shell que estamos usando? Com usurário root: >>> echo $SHELL
+# Se não estiver no bash, é só chamá-lo: >>> bash
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+#
+#
+#
+#
