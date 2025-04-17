4768bc95914d3674fb2f987579ed1a5e2bccc47f
Cristiano Roberto <cristianoroberto@outlook.com>
2025-04-17 16:56:41 -0300

    Anotações de estudo 6

--- notas_de_aprendizado.sh
+++ notas_de_aprendizado.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# - - - - - - - - - - - - - - - - - N O T A S   D E   E S T U D O - - - - - - - - - - - - - - - - - - - - - - - - -
-# - - - - - - - - Certificação Linux: Guia Prático Para a Prova LPIC-1 101 (Juliano Ramos) - - - - - - - - - - -
+# - - - - - - - - - - - - - - - - - N O T A S   D E   E S T U D O - - - - - - - - - - - - - - - - - - - - - - - 
+# - - - - - - - Certificação Linux: Guia Prático Para a Prova LPIC-1 101 (Juliano Ramos) - - - - - - - - - - - 
 #
 # 1) USUÁRIOS - Na linha de comando o símbolo "$" representa o usuário padrão, e "#" o usuário administrador. 
 # Para alterarmos para o usuário administrador, basta digitar: >>> su root
 echo "Comando: su root"
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 # 2) SENHAS - Ao trocar para usuário será necessária a sua senha de administrador. Se você usa Ubuntu linux ou 
 # outra distribuição que não tem o usuário root disponível por padrão, execute o comando: >>> sudo passwd root
 echo "Comando: sudo passwd root"
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 # 3) SHELL - Como saber qual o Shell que estamos usando? Com usuário root: >>> echo $SHELL
 # Se não estiver no bash, é só chamá-lo: >>> bash
 echo "Comando: bash"
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 # 4) COMANDOS EM MODO SEQUENCIAL - Podem ser executados com ou sem condição:
 # 4.1) Sem condição:
 echo "clear; date; ls"
